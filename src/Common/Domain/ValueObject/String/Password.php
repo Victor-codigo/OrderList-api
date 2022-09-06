3c7e2c93139c68e77e5d3bcf2c65caf59f57d2e2
Victor <victorrmmb@hotmail.com>
2022-09-06 13:06:12 +0200

    Added value object password

---
+++ src/Common/Domain/ValueObject/String/Password.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Common\Domain\ValueObject\String;
+
+use Common\Domain\Validation\ConstraintFactory;
+
+class Password extends StringValueObject
+{
+    public const PASSWORD_MIN_CHARS = 6;
+    public const PASSWORD_MAX_CHARS = 256;
+
+    public function __construct(string $value)
+    {
+        parent::__construct($value);
+    }
+
+    public function defineConstraints(): void
+    {
+        $this
+            ->setConstraint(ConstraintFactory::notBlank())
+            ->setConstraint(ConstraintFactory::stringRange(self::PASSWORD_MIN_CHARS, self::PASSWORD_MAX_CHARS));
+    }
+}
---
+++ tests/Unit/Common/Domain/ValueObject/String/PasswordTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Common\Domain\ValueObject\String;
+
+use Common\Adapter\Validation\ValidationChain;
+use Common\Domain\Validation\IValidation;
+use Common\Domain\Validation\VALIDATION_ERRORS;
+use Common\Domain\ValueObject\String\Password;
+use PHPUnit\Framework\TestCase;
+
+class PasswordTest extends TestCase
+{
+    private IValidation $validation;
+    private string $validPassword = '123456';
+
+    public function setUp(): void
+    {
+        $this->validation = new ValidationChain();
+    }
+
+    private function createPassword($password)
+    {
+        return new Password($password);
+    }
+
+    public function testPasswordOk()
+    {
+        $password = $this->createPassword($this->validPassword);
+        $return = $this->validation->validateValueObject($password);
+
+        $this->assertEmpty($return,
+            'It was expected that doesnt return errors');
+    }
+
+    public function testPasswordNotBlankAndShort()
+    {
+        $password = $this->createPassword('');
+        $return = $this->validation->validateValueObject($password);
+
+        $this->assertEquals([VALIDATION_ERRORS::NOT_BLANK, VALIDATION_ERRORS::STRING_TOO_SHORT], $return,
+            'It was expected that returns errors: [VALIDATION_ERRORS::NOT_BLANK, VALIDATION_ERRORS::STRING_TOO_SHORT]');
+    }
+
+    public function testPasswordNotTooLong()
+    {
+        $password = $this->createPassword(str_repeat('-', Password::PASSWORD_MAX_CHARS + 1));
+        $return = $this->validation->validateValueObject($password);
+
+        $this->assertEquals([VALIDATION_ERRORS::STRING_TOO_LONG], $return,
+            'It was expected that returns errors: [VALIDATION_ERRORS::STRING_TOO_SHORT]');
+    }
+}
