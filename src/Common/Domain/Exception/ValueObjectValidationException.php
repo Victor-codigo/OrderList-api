346f37e28eced31fcd4adcecb689f591ae520eda
Victor <victorrmmb@hotmail.com>
2022-09-16 13:03:34 +0200

    added exceptions for properties and validation

---
+++ src/Common/Domain/Exception/ValueObjectValidationException.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Common\Domain\Exception;
+
+use Common\Domain\Validation\VALIDATION_ERRORS;
+use DomainException;
+
+class ValueObjectValidationException extends DomainException
+{
+    private function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    public static function create(string $message): self
+    {
+        return new self($message);
+    }
+
+    /**
+     * @param VALIDATION_ERRORS[] $errors
+     */
+    public static function createFromArray(array $errors): self
+    {
+        $errorsNames = array_map(fn (VALIDATION_ERRORS $error) => $error->name, $errors);
+
+        return self::create(sprintf(
+            'ValueObject validation errors [%s]',
+            implode(', ', $errorsNames)
+        ));
+    }
+}
