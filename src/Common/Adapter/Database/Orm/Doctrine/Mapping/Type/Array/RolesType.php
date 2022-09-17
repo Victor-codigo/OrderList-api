ecfb212806e4d204ce9c95b54e2b572ac826d84b
Victor <victorrmmb@hotmail.com>
2022-09-17 10:57:54 +0200

    Added type RolesType as data base type

---
+++ src/Common/Adapter/Database/Orm/Doctrine/Mapping/Type/Array/RolesType.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Common\Adapter\Database\Orm\Doctrine\Mapping\Type\array;
+
+use Common\Adapter\Database\Orm\Doctrine\Mapping\Type\TypeBase;
+use Common\Domain\Model\ValueObject\array\Roles;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+
+class RolesType extends TypeBase
+{
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return 'JSON';
+    }
+
+    public function getClassImplementationName(): string
+    {
+        return Roles::class;
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): mixed
+    {
+        return json_encode(
+            parent::convertToDatabaseValue($value, $platform),
+            JSON_THROW_ON_ERROR
+        );
+    }
+}
