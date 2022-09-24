8e12a67de9c03763042f8ca292b345a5dfb390f4
Victor <victorrmmb@hotmail.com>
2022-09-24 17:05:41 +0200

    Added service SendEmailRegisterConfirmService

---
+++ src/User/Domain/Service/SendEmailRegisterConfirmation/Dto/SendEmailRegisterConfirmInputDto.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace User\Domain\Service\SendEmailRegisterConfirmation\Dto;
+
+class SendEmailRegisterConfirmInputDto
+{
+    public readonly string $to;
+}
