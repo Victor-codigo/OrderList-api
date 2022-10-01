42930da1ac9c3dd1de33bb42be638e34d9a6cd32
Víctor <vrmmb@hotmail.com>
2022-10-01 14:49:14 +0200

    Added Translator test

---
+++ src/Common/Adapter/Translator/TranslatorSymfonyAdapter.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Common\Adapter\Translator;
+
+use Common\Domain\Exception\InvalidArgumentException;
+use Common\Domain\Ports\Translator\TranslatorInterface;
+use Symfony\Component\Translation\LocaleSwitcher;
+use Symfony\Contracts\Translation\TranslatorInterface as SymfonyTranslator;
+
+class TranslatorSymfonyAdapter implements TranslatorInterface
+{
+    private SymfonyTranslator $translator;
+    private LocaleSwitcher $localeSwitcher;
+
+    public function __construct(SymfonyTranslator $translator, LocaleSwitcher $localeSwitcher)
+    {
+        $this->translator = $translator;
+        $this->localeSwitcher = $localeSwitcher;
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function translate(string $id, array $params = [], string|null $domain = null, string|null $locale = null): string
+    {
+        try {
+            return $this->translator->trans($id, $params, $domain, $locale);
+        } catch (\InvalidArgumentException $e) {
+            throw InvalidArgumentException::createFromMessage($e->getMessage());
+        }
+    }
+
+    public function setLocale(string $locale): void
+    {
+        $this->localeSwitcher->setLocale($locale);
+    }
+
+    public function getLocale(): string
+    {
+        return $this->translator->getLocale();
+    }
+
+    public function resetLocale(): void
+    {
+        $this->localeSwitcher->reset();
+    }
+
+    public function runWithLocale(string $locale, callable $callback): void
+    {
+        $this->localeSwitcher->runWithLocale($locale, $callback);
+    }
+}
---
+++ tests/Unit/Common/Adapter/Translator/TranslatorSymfonyAdapterTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Common\Adapter\Translator;
+
+use Common\Adapter\Translator\TranslatorSymfonyAdapter;
+use Common\Domain\Exception\InvalidArgumentException;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Translation\LocaleSwitcher;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class TranslatorSymfonyAdapterTest extends TestCase
+{
+    private TranslatorSymfonyAdapter $object;
+    private MockObject|TranslatorInterface $translator;
+    private MockObject|LocaleSwitcher $localeSwitcher;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->localeSwitcher = $this->createMock(LocaleSwitcher::class);
+        $this->translator = $this->getMockForAbstractClass(TranslatorInterface::class);
+        $this->object = new TranslatorSymfonyAdapter($this->translator, $this->localeSwitcher);
+    }
+
+    /** @test */
+    public function translateIdentifier(): void
+    {
+        $id = 'identifier';
+        $params = ['param' => 'hola'];
+        $domain = 'domain';
+        $locale = 'locale';
+        $textTranslated = 'This has been translated';
+
+        $this->translator
+            ->expects($this->once())
+            ->method('trans')
+            ->with($id, $params, $domain, $locale)
+            ->willReturn($textTranslated);
+
+        $return = $this->object->translate($id, $params, $domain, $locale);
+
+        $this->assertEquals($textTranslated, $return);
+    }
+
+    /** @test */
+    public function translateThrowsInvalidArgumentException(): void
+    {
+        $id = 'identifier';
+        $params = ['param' => 'hola'];
+        $domain = 'domain';
+        $locale = 'locale';
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->translator
+            ->expects($this->once())
+            ->method('trans')
+            ->with($id, $params, $domain, $locale)
+            ->willThrowException(new \InvalidArgumentException());
+
+        $this->object->translate($id, $params, $domain, $locale);
+    }
+}
