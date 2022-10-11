0ba31d4181332fdd2785da9a78c9f6f9fdb291df
Víctor <vrmmb@hotmail.com>
2022-10-11 14:09:54 +0200

    Changed inheritance Added function fromMessage

--- src/Common/Domain/Exception/DtoInvalidPropertyException.php
+++ src/Common/Domain/Exception/DtoInvalidPropertyException.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Common\Domain\Exception;
 
+use Throwable;
+
 class DtoInvalidPropertyException extends LogicException
 {
+    public static function fromMessage(string $message = '', int $code = 0, Throwable|null $previous = null): static
+    {
+        return new static($message, $code, $previous);
+    }
 }
