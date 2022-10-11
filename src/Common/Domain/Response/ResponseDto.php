4c8d1aff3816d0063017b90751671430112cd807
Víctor <vrmmb@hotmail.com>
2022-10-11 14:10:10 +0200

    Added getters and setters

--- src/Common/Domain/Response/ResponseDto.php
+++ src/Common/Domain/Response/ResponseDto.php
@@ -6,10 +6,58 @@ namespace Common\Domain\Response;
 
 class ResponseDto
 {
-    public readonly RESPONSE_STATUS $status;
-    public readonly string $message;
-    public readonly array $data;
-    public readonly array $errors;
+    public RESPONSE_STATUS $status;
+    public string $message;
+    public array $data;
+    public array $errors;
+
+    public function getStatus(): RESPONSE_STATUS
+    {
+        return $this->status;
+    }
+
+    public function setStatus(RESPONSE_STATUS $status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function setData(array $data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    public function getErrors(): array
+    {
+        return $this->errors;
+    }
+
+    public function setErrors(array $errors)
+    {
+        $this->errors = $errors;
+
+        return $this;
+    }
 
     public function __construct(array $data = [], array $errors = [], string $message = '', RESPONSE_STATUS $status = RESPONSE_STATUS::OK)
     {
