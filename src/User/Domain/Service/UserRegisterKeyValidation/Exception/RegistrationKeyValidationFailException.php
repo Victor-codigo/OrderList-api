bab24a86b6e41afb20556d7a59d724fc29335c49
Víctor <vrmmb@hotmail.com>
2022-10-11 14:02:06 +0200

    Moved RegisterKeyValidationFailException to services exception path

---
+++ src/User/Domain/Service/UserRegisterKeyValidation/Exception/RegistrationKeyValidationFailException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace User\Domain\Service\UserRegisterKeyValidation\Exception;
+
+use Common\Domain\Exception\DomainException;
+
+class RegistrationKeyValidationFailException extends DomainException
+{
+}
