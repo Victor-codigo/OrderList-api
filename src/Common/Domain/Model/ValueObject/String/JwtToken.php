b1fb38d9f8697d74616506c9062379b36c1e5fe5
Víctor <vrmmb@hotmail.com>
2022-10-23 11:04:31 +0200

    Created value object JwtToken

---
+++ src/Common/Domain/Model/ValueObject/String/JwtToken.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Common\Domain\Model\ValueObject\String;
+
+use Common\Domain\Model\ValueObject\Constraints\VALUE_OBJECTS_CONSTRAINTS;
+use Common\Domain\Validation\ConstraintFactory;
+
+class JwtToken extends StringValueObject
+{
+    protected function defineConstraints(): void
+    {
+        $this
+            ->setConstraint(ConstraintFactory::notBlank())
+            ->setConstraint(ConstraintFactory::notNull())
+            ->setConstraint(ConstraintFactory::stringMin(VALUE_OBJECTS_CONSTRAINTS::JWT_TOKEN_MIN_LENGTH));
+    }
+}
