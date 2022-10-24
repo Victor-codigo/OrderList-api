7dffecae8806850829292b28301caea031cde8b2
Víctor <vrmmb@hotmail.com>
2022-10-24 13:27:07 +0200

    Create properties objectManager and entityManager

--- src/Common/Adapter/Database/Orm/Doctrine/Repository/RepositoryBase.php
+++ src/Common/Adapter/Database/Orm/Doctrine/Repository/RepositoryBase.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Common\Adapter\Database\Orm\Doctrine\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Uid\Uuid;
@@ -12,12 +13,14 @@ use Symfony\Component\Uid\Uuid;
 abstract class RepositoryBase extends ServiceEntityRepository
 {
     protected ObjectManager $objectManager;
+    protected EntityManagerInterface $entityManager;
 
-    public function __construct(ManagerRegistry $managerRegistry, string $entity)
+    public function __construct(ManagerRegistry $managerRegistry, string $entityClass)
     {
-        parent::__construct($managerRegistry, $entity);
+        parent::__construct($managerRegistry, $entityClass);
 
         $this->objectManager = $managerRegistry->getManager();
+        $this->entityManager = $this->getEntityManager();
     }
 
     public function generateId(): string
