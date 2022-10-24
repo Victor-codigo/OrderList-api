2b7a1eb8dc0c2a83705dcc49d9444c2b600d6af6
Víctor <vrmmb@hotmail.com>
2022-10-24 13:27:59 +0200

    DBNotFoundException, now extends DomainException

--- src/Common/Domain/Database/Orm/Doctrine/Repository/Exception/DBNotFoundException.php
+++ src/Common/Domain/Database/Orm/Doctrine/Repository/Exception/DBNotFoundException.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Common\Domain\Database\Orm\Doctrine\Repository\Exception;
 
+use Common\Domain\Exception\DomainException;
 use Throwable;
 
-class DBNotFoundException implements DBExceptionInterface
+class DBNotFoundException extends DomainException implements DBExceptionInterface
 {
     public static function fromMessage(string $message = '', int $code = 0, Throwable|null $previous = null): static
     {
