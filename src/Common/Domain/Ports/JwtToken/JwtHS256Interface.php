0c8b2971b3ef938e14e40a7dabf4f0e0fd0b6c7d
Víctor <vrmmb@hotmail.com>
2022-11-08 16:16:55 +0100

    Removed userPasswordRememberChangeConfirm

--- src/Common/Domain/Ports/JwtToken/JwtHS256Interface.php
+++ src/Common/Domain/Ports/JwtToken/JwtHS256Interface.php
@@ -8,6 +8,9 @@ interface JwtHS256Interface
 {
     public function encode(array $data, float $expire = 3600): string;
 
+    /**
+     * @throws JwtException
+     */
     public function decode(string $token): object;
 
     public function hasExpired(object $tokenDecoded): bool;
--- src/User/Application/UserPasswordRememberChange/Exception/UserPasswordRememberChangePasswordNewAndPasswrodNewRepeatAreNotEqualsException.php
+++ src/User/Application/UserPasswordRememberChange/Exception/UserPasswordRememberChangePasswordNewAndPasswrodNewRepeatAreNotEqualsException.php
@@ -2,13 +2,13 @@
 
 declare(strict_types=1);
 
-namespace App\User\Application\UserPasswordRememberChange\Exception;
+namespace User\Application\UserPasswordRememberChange\Exception;
 
 use Common\Domain\Exception\DomainExceptionOutput;
 use Common\Domain\Response\RESPONSE_STATUS;
 use Common\Domain\Response\RESPONSE_STATUS_HTTP;
 
-class UserPasswordRememberChangePasswordNewAndPasswrodNewRememberAreNotEqualsException extends DomainExceptionOutput
+class UserPasswordRememberChangePasswordNewAndPasswrodNewRepeatAreNotEqualsException extends DomainExceptionOutput
 {
     public static function fromMessage(string $message): static
     {
