e826045d5fa5bc6ef057b6b1c4b98574ffa2d0ab
Víctor <vrmmb@hotmail.com>
2022-11-09 18:55:02 +0100

    Added test for value object JwtToken

---
+++ tests/Unit/Common/Domain/Model/ValueObject/String/JwtTokenTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Common\Domain\Model\ValueObject\String;
+
+use Common\Adapter\Jwt\JwtLexikAdapter;
+use Common\Adapter\Validation\ValidationChain;
+use Common\Domain\Model\ValueObject\String\JwtToken;
+use Common\Domain\Validation\VALIDATION_ERRORS;
+use Common\Domain\Validation\ValidationInterface;
+use PHPUnit\Framework\TestCase;
+
+class JwtTokenTest extends TestCase
+{
+    private ValidationInterface $validator;
+    private const PATH_PRIVATE_KEY = 'tests/Fixtures/JwtKey/private.pem';
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->validator = new ValidationChain();
+    }
+
+    private function createToken(array $data = []): JwtToken
+    {
+        $encoder = new JwtLexikAdapter(file_get_contents(self::PATH_PRIVATE_KEY));
+        $token = $encoder->encode($data);
+
+        return new JwtToken($token);
+    }
+
+    /** @test */
+    public function tokenOk()
+    {
+        $token = $this->createToken();
+        $return = $this->validator->validateValueObject($token);
+
+        $this->assertEmpty($return);
+    }
+
+    /** @test */
+    public function tokenNotBlank()
+    {
+        $return = $this->validator->validateValueObject(new JwtToken(''));
+
+        $this->assertEquals([VALIDATION_ERRORS::NOT_BLANK, VALIDATION_ERRORS::STRING_TOO_SHORT], $return);
+    }
+
+    /** @test */
+    public function tokenNotNull()
+    {
+        $return = $this->validator->validateValueObject(new JwtToken(null));
+
+        $this->assertEquals([VALIDATION_ERRORS::NOT_BLANK, VALIDATION_ERRORS::NOT_NULL], $return);
+    }
+
+    /** @test */
+    public function tokenTooShort()
+    {
+        $return = $this->validator->validateValueObject(new JwtToken(str_pad('', 35, '-')));
+
+        $this->assertEquals([VALIDATION_ERRORS::STRING_TOO_SHORT], $return);
+    }
+}
