37d79b8d1a4cd43dcd52fba8812e5d1240bb3914
Victor <victorrmmb@hotmail.com>
2023-01-30 19:29:29 +0100

    Added limit to number users in GroupUsersAdd

--- src/Group/Adapter/Http/Controller/GroupUserAdd/Dto/GroupUserAddRequestDto.php
+++ src/Group/Adapter/Http/Controller/GroupUserAdd/Dto/GroupUserAddRequestDto.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 class GroupUserAddRequestDto implements RequestDtoInterface
 {
+    private const USERS_NUM_MAX = 50;
+
     public readonly string|null $groupId;
     public readonly array|null $usersId;
     public readonly bool|null $admin;
@@ -16,7 +18,23 @@ class GroupUserAddRequestDto implements RequestDtoInterface
     public function __construct(Request $request)
     {
         $this->groupId = $request->request->get('group_id');
-        $this->usersId = $request->request->get('users');
+        $this->usersId = $this->removeUsersOverflow($request->request->get('users'));
         $this->admin = $request->request->get('admin');
+    }
+
+    /**
+     * @param string[]|null $usersId
+     */
+    private function removeUsersOverflow(array|null $usersId): array|null
+    {
+        if (null === $usersId) {
+            return null;
+        }
+
+        if (count($usersId) > self::USERS_NUM_MAX) {
+            return array_slice($usersId, 0, self::USERS_NUM_MAX);
+        }
+
+        return $usersId;
     }
 }
