cbba448d1423f64ff2417ec1443720ee768aeb08
Victor <victorrmmb@hotmail.com>
2023-02-02 12:34:09 +0100

    docs: :memo: Added excetions to DIInterface documentation
    Added Exception documentation for interface DIInterface, for methodsgetUrlRouteAbsolute and getUrlRouteRelative

--- src/Common/Domain/Ports/DI/DIInterface.php
+++ src/Common/Domain/Ports/DI/DIInterface.php
@@ -6,8 +6,18 @@ namespace Common\Domain\Ports\DI;
 
 interface DIInterface
 {
+    /**
+     * @throws RouteNotFoundException
+     * @throws RouteParametersMissingException
+     * @throws RouteInvalidParameterException
+     */
     public function getUrlRouteAbsolute(string $route, array $params): string;
 
+    /**
+     * @throws RouteNotFoundException
+     * @throws RouteParametersMissingException
+     * @throws RouteInvalidParameterException
+     */
     public function getUrlRouteRelative(string $route, array $params): string;
 
     public function getLocale(): string;
