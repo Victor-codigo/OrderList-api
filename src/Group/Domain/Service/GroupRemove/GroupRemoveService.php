d5d529be739943d4d867a2fcf73e8a428f3cb90e
Víctor <victorrmmb@hotmail.xompuf>
2023-03-03 17:53:01 +0100

    refactor: :recycle: Added support for removing group image
    Now the image of the group, is removed with the group

--- src/Group/Domain/Service/GroupRemove/GroupRemoveService.php
+++ src/Group/Domain/Service/GroupRemove/GroupRemoveService.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Group\Domain\Service\GroupRemove;
 
+use Common\Domain\Exception\DomainInternalErrorException;
+use Group\Domain\Model\Group;
 use Group\Domain\Port\Repository\GroupRepositoryInterface;
 use Group\Domain\Service\GroupRemove\Dto\GroupRemoveDto;
 
@@ -11,6 +13,7 @@ class GroupRemoveService
 {
     public function __construct(
         private GroupRepositoryInterface $groupRepository,
+        private string $groupImagePath
     ) {
     }
 
@@ -22,6 +25,29 @@ class GroupRemoveService
     {
         $group = $this->groupRepository->findGroupsByIdOrFail([$input->groupId]);
 
+        $this->removeGroupImage($group[0]);
         $this->groupRepository->remove($group[0]);
+    }
+
+    /**
+     * @throws DomainInternalErrorException
+     */
+    private function removeGroupImage(Group $group): void
+    {
+        $image = $group->getImage()?->getValue();
+
+        if (null === $image) {
+            return;
+        }
+
+        $image = $this->groupImagePath.'/'.$image;
+
+        if (!file_exists($image)) {
+            return;
+        }
+
+        if (!unlink($image)) {
+            throw DomainInternalErrorException::fromMessage('The image cannot be deleted');
+        }
     }
 }
--- tests/Unit/Group/Domain/Service/GroupRemove/GroupRemoveServiceTest.php
+++ tests/Unit/Group/Domain/Service/GroupRemove/GroupRemoveServiceTest.php
@@ -6,18 +6,22 @@ namespace Test\Unit\Group\Domain\Service\GroupRemove;
 
 use Common\Domain\Database\Orm\Doctrine\Repository\Exception\DBConnectionException;
 use Common\Domain\Database\Orm\Doctrine\Repository\Exception\DBNotFoundException;
+use Common\Domain\Exception\DomainInternalErrorException;
 use Common\Domain\Model\ValueObject\ValueObjectFactory;
 use Group\Domain\Model\GROUP_TYPE;
 use Group\Domain\Model\Group;
 use Group\Domain\Port\Repository\GroupRepositoryInterface;
+use Group\Domain\Service\GroupRemove\BuiltInFunctionsReturn;
 use Group\Domain\Service\GroupRemove\Dto\GroupRemoveDto;
 use Group\Domain\Service\GroupRemove\GroupRemoveService;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
+require_once 'tests/BuiltinFunctions/GroupRemoveService.php';
 class GroupRemoveServiceTest extends TestCase
 {
     private const GROUP_ID = 'fdb242b4-bac8-4463-88d0-0941bb0beee0';
+    private const PATH_GROUP_IMAGES = 'path/to/group/images';
 
     private GroupRemoveService $object;
     private MockObject|GroupRepositoryInterface $groupRepository;
@@ -27,7 +31,7 @@ class GroupRemoveServiceTest extends TestCase
         parent::setUp();
 
         $this->groupRepository = $this->createMock(GroupRepositoryInterface::class);
-        $this->object = new GroupRemoveService($this->groupRepository);
+        $this->object = new GroupRemoveService($this->groupRepository, self::PATH_GROUP_IMAGES);
     }
 
     private function getGroup(): Group
@@ -53,6 +57,54 @@ class GroupRemoveServiceTest extends TestCase
             ->method('remove')
             ->with($group);
 
+        $this->object->__invoke($input);
+    }
+
+    /** @test */
+    public function itShouldRemoveTheGroupImageNotExists(): void
+    {
+        $groupId = ValueObjectFactory::createIdentifier(self::GROUP_ID);
+        $group = $this->getGroup();
+        $group->setImage(ValueObjectFactory::createPath('image.png'));
+        $input = new GroupRemoveDto($groupId);
+
+        $this->groupRepository
+            ->expects($this->once())
+            ->method('findGroupsByIdOrFail')
+            ->with([$groupId])
+            ->willReturn([$group]);
+
+        $this->groupRepository
+            ->expects($this->once())
+            ->method('remove')
+            ->with($group);
+
+        BuiltInFunctionsReturn::$file_exists = false;
+        $this->object->__invoke($input);
+    }
+
+    /** @test */
+    public function itShouldFailTheGroupCanNotRemoveGroupImage(): void
+    {
+        $groupId = ValueObjectFactory::createIdentifier(self::GROUP_ID);
+        $group = $this->getGroup();
+        $group->setImage(ValueObjectFactory::createPath('image.png'));
+        $input = new GroupRemoveDto($groupId);
+
+        $this->groupRepository
+            ->expects($this->once())
+            ->method('findGroupsByIdOrFail')
+            ->with([$groupId])
+            ->willReturn([$group]);
+
+        $this->groupRepository
+            ->expects($this->never())
+            ->method('remove');
+
+        BuiltInFunctionsReturn::$file_exists = true;
+        BuiltInFunctionsReturn::$unlink = false;
+
+        $this->expectException(DomainInternalErrorException::class);
         $this->object->__invoke($input);
     }
 
