f08dc549612a746cea8b420d8fd17462e2eed1d6
Víctor <victorrmmb@hotmail.xompuf>
2023-03-21 17:44:56 +0100

    refactor: :recycle: Added rol USER_FIRST_LOGIN

--- src/User/Domain/Model/USER_ROLES.php
+++ src/User/Domain/Model/USER_ROLES.php
@@ -9,5 +9,6 @@ enum USER_ROLES: string
     case USER = 'ROLE_USER';
     case ADMIN = 'ROLE_ADMIN';
     case NOT_ACTIVE = 'ROLE_NOT_ACTIVE';
+    case USER_FIRST_LOGIN = 'ROLE_USER_FIRST_LOGIN';
     case DELETED = 'ROLE_DELETED';
 }
