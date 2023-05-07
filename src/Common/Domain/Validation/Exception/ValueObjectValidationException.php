09c54de9cb08672dcec60b4e2a26ed066e045cf4
Víctor <victorrmmb@hotmail.com>
2023-05-07 15:34:30 +0200

    refactor: :recycle: Changed ValueObjectValidationException::fromArray, now admit and array of arrays of errors

--- src/Common/Domain/Validation/Exception/ValueObjectValidationException.php
+++ src/Common/Domain/Validation/Exception/ValueObjectValidationException.php
@@ -19,12 +19,25 @@ class ValueObjectValidationException extends DomainExceptionOutput implements Va
         $errorValueObject = [];
 
         foreach ($errors as $valueObjectName => $errorList) {
-            $errorValueObject[$valueObjectName] = array_map(
-                fn (VALIDATION_ERRORS $error) => mb_strtolower($error->name),
-                $errorList
-            );
+            $errorValueObject[$valueObjectName] = self::errorsToLower($errorList);
         }
 
         return new static($message, $errorValueObject, RESPONSE_STATUS::ERROR, RESPONSE_STATUS_HTTP::BAD_REQUEST);
+    }
+
+    private static function errorsToLower(array $errorList): array
+    {
+        $errorListToLower = [];
+        foreach ($errorList as $key => $error) {
+            if (is_array($error)) {
+                $errorListToLower[$key] = self::errorsToLower($error);
+
+                continue;
+            }
+
+            $errorListToLower[$key] = mb_strtolower($error->name);
+        }
+
+        return $errorListToLower;
     }
 }
---
+++ tests/Unit/Common/Domain/Validation/Exception/ValueObjectValidationExceptionTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Unit\Common\Domain\Validation\Exception;
+
+use Common\Domain\Response\RESPONSE_STATUS_HTTP;
+use Common\Domain\Validation\Common\VALIDATION_ERRORS;
+use Common\Domain\Validation\Exception\ValueObjectValidationException;
+use PHPUnit\Framework\TestCase;
+
+class ValueObjectValidationExceptionTest extends TestCase
+{
+    /** @test */
+    public function isShouldCreateTheExceptionFromAMessageAndAListOfErrors(): void
+    {
+        $message = 'message';
+        $errors = [
+            'key 1' => [VALIDATION_ERRORS::ALPHANUMERIC, VALIDATION_ERRORS::ALPHANUMERIC_WITH_WHITESPACE],
+            'key 2' => [VALIDATION_ERRORS::CHOICE_NOT_SUCH, VALIDATION_ERRORS::CHOICE_TOO_FEW],
+            'key 3' => [VALIDATION_ERRORS::DATE_INVALID_FORMAT, VALIDATION_ERRORS::DATE_INVALID],
+        ];
+        $errorsExpected = [
+            'key 1' => [mb_strtolower(VALIDATION_ERRORS::ALPHANUMERIC->name), mb_strtolower(VALIDATION_ERRORS::ALPHANUMERIC_WITH_WHITESPACE->name)],
+            'key 2' => [mb_strtolower(VALIDATION_ERRORS::CHOICE_NOT_SUCH->name), mb_strtolower(VALIDATION_ERRORS::CHOICE_TOO_FEW->name)],
+            'key 3' => [mb_strtolower(VALIDATION_ERRORS::DATE_INVALID_FORMAT->name), mb_strtolower(VALIDATION_ERRORS::DATE_INVALID->name)],
+        ];
+
+        $return = ValueObjectValidationException::fromArray($message, $errors);
+
+        $this->assertEquals(RESPONSE_STATUS_HTTP::BAD_REQUEST, $return->httpStatus);
+        $this->assertEquals($errorsExpected, $return->getErrors());
+    }
+
+    /** @test */
+    public function isShouldCreateTheExceptionFromAMessageAndAListOfGroupsOfErrors(): void
+    {
+        $message = 'message';
+        $errors = [
+            'key 1' => [
+                [VALIDATION_ERRORS::ALPHANUMERIC, VALIDATION_ERRORS::ALPHANUMERIC_WITH_WHITESPACE],
+                [VALIDATION_ERRORS::ALPHANUMERIC, VALIDATION_ERRORS::ALPHANUMERIC_WITH_WHITESPACE],
+            ],
+            'key 2' => [
+                [VALIDATION_ERRORS::CHOICE_NOT_SUCH, VALIDATION_ERRORS::CHOICE_TOO_FEW],
+                [VALIDATION_ERRORS::CHOICE_NOT_SUCH, VALIDATION_ERRORS::CHOICE_TOO_FEW],
+            ],
+            'key 3' => [
+                [VALIDATION_ERRORS::DATE_INVALID_FORMAT, VALIDATION_ERRORS::DATE_INVALID],
+                [VALIDATION_ERRORS::DATE_INVALID_FORMAT, VALIDATION_ERRORS::DATE_INVALID],
+            ],
+        ];
+        $errorsExpected = [
+            'key 1' => [
+                [mb_strtolower(VALIDATION_ERRORS::ALPHANUMERIC->name), mb_strtolower(VALIDATION_ERRORS::ALPHANUMERIC_WITH_WHITESPACE->name)],
+                [mb_strtolower(VALIDATION_ERRORS::ALPHANUMERIC->name), mb_strtolower(VALIDATION_ERRORS::ALPHANUMERIC_WITH_WHITESPACE->name)],
+            ],
+            'key 2' => [
+                [mb_strtolower(VALIDATION_ERRORS::CHOICE_NOT_SUCH->name), mb_strtolower(VALIDATION_ERRORS::CHOICE_TOO_FEW->name)],
+                [mb_strtolower(VALIDATION_ERRORS::CHOICE_NOT_SUCH->name), mb_strtolower(VALIDATION_ERRORS::CHOICE_TOO_FEW->name)],
+            ],
+            'key 3' => [
+                [mb_strtolower(VALIDATION_ERRORS::DATE_INVALID_FORMAT->name), mb_strtolower(VALIDATION_ERRORS::DATE_INVALID->name)],
+                [mb_strtolower(VALIDATION_ERRORS::DATE_INVALID_FORMAT->name), mb_strtolower(VALIDATION_ERRORS::DATE_INVALID->name)],
+            ],
+        ];
+
+        $return = ValueObjectValidationException::fromArray($message, $errors);
+
+        $this->assertEquals(RESPONSE_STATUS_HTTP::BAD_REQUEST, $return->httpStatus);
+        $this->assertEquals($errorsExpected, $return->getErrors());
+    }
+}
