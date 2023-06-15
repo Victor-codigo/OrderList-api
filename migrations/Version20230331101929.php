cb3a89fc2ce354bf12107d8d26b2853f06c2a9ad
Víctor <victorrmmb@hotmail.com>
2023-06-15 11:47:39 +0200

    refactor: :card_file_box: Fixed Table Notifications, field data, changed to nullable

--- migrations/Version20230331101929.php
+++ migrations/Version20230331101929.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230331101929 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,7 +16,7 @@ final class Version20230331101929 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE Notifications (id CHAR(36) NOT NULL, user_id CHAR(36) NOT NULL, type VARCHAR(50) NOT NULL, data JSON, viewed TINYINT(1) NOT NULL, created_on DATETIME NOT NULL, UNIQUE INDEX u_notification_id (id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE Notifications (id CHAR(36) NOT NULL, user_id CHAR(36) NOT NULL, type VARCHAR(50) NOT NULL, data JSON DEFAULT NULL, viewed TINYINT(1) NOT NULL, created_on DATETIME NOT NULL, UNIQUE INDEX u_notification_id (id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
