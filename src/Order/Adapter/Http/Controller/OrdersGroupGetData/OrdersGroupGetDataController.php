8321ae0ca5c7feccee4d6248e5607e9a770fe8ab
Víctor <victorrmmb@hotmail.com>
2023-10-26 12:40:56 +0200

    docs: :memo: Added to OrdersGroupGetDataController OpenApi documentation

--- src/Order/Adapter/Http/Controller/OrdersGroupGetData/OrdersGroupGetDataController.php
+++ src/Order/Adapter/Http/Controller/OrdersGroupGetData/OrdersGroupGetDataController.php
@@ -7,6 +7,7 @@ namespace Order\Adapter\Http\Controller\OrdersGroupGetData;
 use Common\Domain\Application\ApplicationOutputInterface;
 use Common\Domain\Response\RESPONSE_STATUS;
 use Common\Domain\Response\ResponseDto;
+use OpenApi\Attributes as OA;
 use Order\Adapter\Http\Controller\OrdersGroupGetData\Dto\OrdersGroupGetDataRequestDto;
 use Order\Application\OrdersGroupGetData\Dto\OrdersGroupGetDataInputDto;
 use Order\Application\OrdersGroupGetData\OrdersGroupGetDataUseCase;
@@ -15,6 +16,94 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
+#[OA\Tag('Order')]
+#[OA\Get(
+    description: 'Get a list of orders from a group',
+    parameters: [
+        new OA\Parameter(
+            name: 'group_id',
+            in: 'path',
+            required: true,
+            description: 'Group id',
+            example: '5483539d-52f7-4aa9-a91c-1aae11c3d17f',
+            schema: new OA\Schema(type: 'string')
+        ),
+        new OA\Parameter(
+            name: 'page',
+            in: 'query',
+            required: true,
+            description: 'Number of the page of orders',
+            example: 4,
+            schema: new OA\Schema(type: 'int')
+        ),
+        new OA\Parameter(
+            name: 'page_items',
+            in: 'query',
+            required: true,
+            description: 'Number of items in the page',
+            example: 100,
+            schema: new OA\Schema(type: 'int')
+        ),
+    ],
+    responses: [
+        new OA\Response(
+            response: Response::HTTP_OK,
+            description: 'Orders of the group',
+            content: new OA\MediaType(
+                mediaType: 'application/json',
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(property: 'status', type: 'string', example: 'ok'),
+                        new OA\Property(property: 'message', type: 'string', example: 'Order\'s data'),
+                        new OA\Property(property: 'data', type: 'array', items: new OA\Items(
+                            properties: [
+                                new OA\Property(property: 'id', type: 'string'),
+                                new OA\Property(property: 'user_id', type: 'string'),
+                                new OA\Property(property: 'group_id', type: 'string'),
+                                new OA\Property(property: 'description', type: 'string'),
+                                new OA\Property(property: 'amount', type: 'float'),
+                                new OA\Property(property: 'created_on', type: 'datetime'),
+                                new OA\Property(property: 'product', type: 'array', items: new OA\Items(
+                                    properties: [
+                                        new OA\Property(property: 'id', type: 'string'),
+                                        new OA\Property(property: 'name', type: 'string'),
+                                        new OA\Property(property: 'description', type: 'string'),
+                                        new OA\Property(property: 'image', type: 'string'),
+                                        new OA\Property(property: 'created_on', type: 'datetime'),
+                                    ]
+                                )),
+                                new OA\Property(property: 'shop', type: 'array', items: new OA\Items(
+                                    properties: [
+                                        new OA\Property(property: 'id', type: 'string'),
+                                        new OA\Property(property: 'name', type: 'string'),
+                                        new OA\Property(property: 'description', type: 'string'),
+                                        new OA\Property(property: 'image', type: 'string'),
+                                        new OA\Property(property: 'created_on', type: 'datetime'),
+                                    ]
+                                )),
+                            ])),
+                        new OA\Property(property: 'errors', type: 'array', items: new OA\Items()),
+                    ]
+                )
+            )
+        ),
+        new OA\Response(
+            response: Response::HTTP_BAD_REQUEST,
+            description: 'Error retrieving orders',
+            content: new OA\MediaType(
+                mediaType: 'application/json',
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(property: 'status', type: 'string', example: 'error'),
+                        new OA\Property(property: 'message', type: 'string', example: 'Some error message'),
+                        new OA\Property(property: 'data', type: 'array', items: new OA\Items()),
+                        new OA\Property(property: 'errors', type: 'array', items: new OA\Items(default: '<group_id|page|page_items|permissions|list_order_id|orders_not_found, string|array>')),
+                    ]
+                )
+            )
+        ),
+    ]
+)]
 class OrdersGroupGetDataController extends AbstractController
 {
     public function __construct(
