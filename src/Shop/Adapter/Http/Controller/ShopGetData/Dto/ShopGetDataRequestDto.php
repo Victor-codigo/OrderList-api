04a8770ae5e8c3bcaf0ec61fc0fa1bad1f4718af
Víctor <victorrmmb@hotmail.com>
2023-11-27 12:15:24 +0100

    refactor: :recycle: Added to class ShopGetDataRequestDto, trait RequestDataValidation. Removed method removeOverflow

--- src/Shop/Adapter/Http/Controller/ShopGetData/Dto/ShopGetDataRequestDto.php
+++ src/Shop/Adapter/Http/Controller/ShopGetData/Dto/ShopGetDataRequestDto.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Shop\Adapter\Http\Controller\ShopGetData\Dto;
 
 use Common\Adapter\Http\Dto\RequestDtoInterface;
+use Common\Adapter\Http\RequestDataValidation\RequestDataValidation;
 use Common\Domain\Config\AppConfig;
 use Symfony\Component\HttpFoundation\Request;
 
 class ShopGetDataRequestDto implements RequestDtoInterface
 {
+    use RequestDataValidation;
+
     private const SHOPS_NUM_MAX = AppConfig::ENDPOINT_SHOP_GET_SHOPS_MAX;
     private const PRODUCTS_NUM_MAX = AppConfig::ENDPOINT_SHOP_GET_PRODUCTS_MAX;
 
@@ -23,25 +26,10 @@ class ShopGetDataRequestDto implements RequestDtoInterface
     public function __construct(Request $request)
     {
         $this->groupId = $request->query->get('group_id');
-        $this->shopsId = $this->removeOverflow($request->query->get('shops_id'), self::SHOPS_NUM_MAX);
-        $this->productsId = $this->removeOverflow($request->query->get('products_id'), self::PRODUCTS_NUM_MAX);
+        $this->shopsId = $this->validateCsvOverflow($request->query->get('shops_id'), self::SHOPS_NUM_MAX);
+        $this->productsId = $this->validateCsvOverflow($request->query->get('products_id'), self::PRODUCTS_NUM_MAX);
         $this->shopNameStartsWith = $request->query->get('shop_name_starts_with');
         $this->shopName = $request->query->get('shop_name');
         $this->orderArc = $request->query->getBoolean('order_asc', true);
-    }
-
-    private function removeOverflow(string|null $itemsId, int $numMax): array|null
-    {
-        if (null === $itemsId) {
-            return null;
-        }
-
-        $itemsIdValid = explode(',', $itemsId, $numMax + 1);
-
-        if (count($itemsIdValid) > $numMax) {
-            $itemsIdValid = array_slice($itemsIdValid, 0, $numMax);
-        }
-
-        return $itemsIdValid;
     }
 }
