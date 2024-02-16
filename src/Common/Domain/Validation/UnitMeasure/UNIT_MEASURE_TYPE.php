8c72d99bcbdfcf1e843fa0502fe71dc82d74fda8
Victor <victorrmmb@hotmail.com>
2024-02-16 17:21:55 +0100

    refactor: :recycle: Changed Unit of measures UNIT_MEASURE_TYPE and UNIT_MEASURE_TYPES

--- src/Common/Domain/Validation/UnitMeasure/UNIT_MEASURE_TYPE.php
+++ src/Common/Domain/Validation/UnitMeasure/UNIT_MEASURE_TYPE.php
@@ -7,10 +7,18 @@ namespace Common\Domain\Validation\UnitMeasure;
 enum UNIT_MEASURE_TYPE: string
 {
     case UNITS = 'UNITS';
+
     case KG = 'KG';
     case G = 'G';
+    case CG = 'CG';
+
     case M = 'M';
     case DM = 'DM';
     case CM = 'CM';
     case MM = 'MM';
+
+    case L = 'L';
+    case DL = 'DL';
+    case CL = 'CL';
+    case ML = 'ML';
 }
