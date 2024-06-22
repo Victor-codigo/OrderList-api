892328dc3663966c3eb6d80abc09f7ed2435e941
Victor <victorrmmb@hotmail.com>
2024-06-22 13:12:06 +0200

    refactor: :memo: Uncomment endpoint  ProductRemoveAllGroupsProducts documentation

--- src/Product/Adapter/Http/Controller/ProductRemoveAllGroupsProducts/ProductRemoveAllGroupsProductsController.php
+++ src/Product/Adapter/Http/Controller/ProductRemoveAllGroupsProducts/ProductRemoveAllGroupsProductsController.php
@@ -17,71 +17,71 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-// #[OA\Tag('Product')]
-// #[OA\Delete(
-//     description: 'Removes all products of groups passed',
-//     requestBody: new OA\RequestBody(
-//         required: true,
-//         content: [
-//             new OA\MediaType(
-//                 mediaType: 'application/json',
-//                 schema: new OA\Schema(
-//                     properties: [
-//                         new OA\Property(property: 'groups_id', type: 'array', description: 'Groups ids', items: new OA\Items()),
-//                         new OA\Property(property: 'system_key', type: 'string', description: 'Key of the system'),
-//                     ]
-//                 )
-//             ),
-//         ]
-//     ),
-//     responses: [
-//         new OA\Response(
-//             response: Response::HTTP_OK,
-//             description: 'The products have been removed',
-//             content: new OA\MediaType(
-//                 mediaType: 'application/json',
-//                 schema: new OA\Schema(
-//                     properties: [
-//                         new OA\Property(property: 'status', type: 'string', example: 'ok'),
-//                         new OA\Property(property: 'message', type: 'string', example: 'Groups products removed'),
-//                         new OA\Property(property: 'data', type: 'array', items: new OA\Items(default: '<id, array>')),
-//                         new OA\Property(property: 'errors', type: 'array', items: new OA\Items()),
-//                     ]
-//                 )
-//             )
-//         ),
-//         new OA\Response(
-//             response: Response::HTTP_BAD_REQUEST,
-//             description: 'The products could not be removed',
-//             content: new OA\MediaType(
-//                 mediaType: 'application/json',
-//                 schema: new OA\Schema(
-//                     properties: [
-//                         new OA\Property(property: 'status', type: 'string', example: 'error'),
-//                         new OA\Property(property: 'message', type: 'string', example: 'Some error message'),
-//                         new OA\Property(property: 'data', type: 'array', items: new OA\Items()),
-//                         new OA\Property(property: 'errors', type: 'array', items: new OA\Items(default: '<groups_id_empty|groups_id|system_key, string|array>')),
-//                     ]
-//                 )
-//             )
-//         ),
-//         new OA\Response(
-//             response: Response::HTTP_NO_CONTENT,
-//             description: 'Groups has no products',
-//             content: new OA\MediaType(
-//                 mediaType: 'application/json',
-//                 schema: new OA\Schema(
-//                     properties: [
-//                         new OA\Property(property: 'status', type: 'string', example: 'error'),
-//                         new OA\Property(property: 'message', type: 'string', example: 'Products not found'),
-//                         new OA\Property(property: 'data', type: 'array', items: new OA\Items()),
-//                         new OA\Property(property: 'errors', type: 'array', items: new OA\Items()),
-//                     ]
-//                 )
-//             )
-//         ),
-//     ]
-// )]
+#[OA\Tag('Product')]
+#[OA\Delete(
+    description: 'Removes all products of groups passed',
+    requestBody: new OA\RequestBody(
+        required: true,
+        content: [
+            new OA\MediaType(
+                mediaType: 'application/json',
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(property: 'groups_id', type: 'array', description: 'Groups ids', items: new OA\Items()),
+                        new OA\Property(property: 'system_key', type: 'string', description: 'Key of the system'),
+                    ]
+                )
+            ),
+        ]
+    ),
+    responses: [
+        new OA\Response(
+            response: Response::HTTP_OK,
+            description: 'The products have been removed',
+            content: new OA\MediaType(
+                mediaType: 'application/json',
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(property: 'status', type: 'string', example: 'ok'),
+                        new OA\Property(property: 'message', type: 'string', example: 'Groups products removed'),
+                        new OA\Property(property: 'data', type: 'array', items: new OA\Items(default: '<id, array>')),
+                        new OA\Property(property: 'errors', type: 'array', items: new OA\Items()),
+                    ]
+                )
+            )
+        ),
+        new OA\Response(
+            response: Response::HTTP_BAD_REQUEST,
+            description: 'The products could not be removed',
+            content: new OA\MediaType(
+                mediaType: 'application/json',
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(property: 'status', type: 'string', example: 'error'),
+                        new OA\Property(property: 'message', type: 'string', example: 'Some error message'),
+                        new OA\Property(property: 'data', type: 'array', items: new OA\Items()),
+                        new OA\Property(property: 'errors', type: 'array', items: new OA\Items(default: '<groups_id_empty|groups_id|system_key, string|array>')),
+                    ]
+                )
+            )
+        ),
+        new OA\Response(
+            response: Response::HTTP_NO_CONTENT,
+            description: 'Groups has no products',
+            content: new OA\MediaType(
+                mediaType: 'application/json',
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(property: 'status', type: 'string', example: 'error'),
+                        new OA\Property(property: 'message', type: 'string', example: 'Products not found'),
+                        new OA\Property(property: 'data', type: 'array', items: new OA\Items()),
+                        new OA\Property(property: 'errors', type: 'array', items: new OA\Items()),
+                    ]
+                )
+            )
+        ),
+    ]
+)]
 class ProductRemoveAllGroupsProductsController extends AbstractController
 {
     public function __construct(
