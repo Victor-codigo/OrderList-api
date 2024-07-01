9b944c0fd1c0132d62d3390a762ae9d574262ccc
Victor <victorrmmb@hotmail.com>
2024-07-01 11:51:14 +0200

    refactor: :recycle: Remved the deprecated EventSubscriberInterface  by the php attribute AsDoctrineListener

--- src/User/Adapter/Database/Orm/Doctrine/Event/UserRegisteredEventSubscriber.php
+++ src/User/Adapter/Database/Orm/Doctrine/Event/UserRegisteredEventSubscriber.php
@@ -8,21 +8,15 @@ use Common\Domain\Model\ValueObject\String\Email;
 use Common\Domain\Model\ValueObject\String\Identifier;
 use Common\Domain\Model\ValueObject\String\Url;
 use Common\Domain\Model\ValueObject\ValueObjectFactory;
-use Doctrine\Bundle\DoctrineBundle\EventSubscriber\EventSubscriberInterface;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\ORM\Event\PostPersistEventArgs;
 use Doctrine\ORM\Events;
 use User\Domain\Event\UserPreRegistered\UserPreRegisteredEvent;
 use User\Domain\Model\User;
 
-class UserRegisteredEventSubscriber implements EventSubscriberInterface
+#[AsDoctrineListener(Events::postPersist)]
+class UserRegisteredEventSubscriber
 {
-    public function getSubscribedEvents(): array
-    {
-        return [
-            Events::postPersist,
-        ];
-    }
-
     public function postPersist(PostPersistEventArgs $event): void
     {
         /** @var User $user */
