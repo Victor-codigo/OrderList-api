0106997055d1eb7a8730e809a46337214eebf63a
Victor <victorrmmb@hotmail.com>
2024-08-09 18:05:59 +0200

    Updated

--- src/Common/Domain/Config/AppConfig.php
+++ src/Common/Domain/Config/AppConfig.php
@@ -131,7 +131,7 @@ class AppConfig
     /**
      * Module communication proxy URL.
      */
-    public const string MODULE_COMMUNICATION_REQUEST_PROXY = 'http://nginx-api:80';
+    public const string MODULE_COMMUNICATION_REQUEST_PROXY = 'http://nginx-api:8082';
 
     /**
      * Module communication HTTPS.
